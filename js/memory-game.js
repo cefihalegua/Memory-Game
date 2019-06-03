9a221fbbf0fe7eae50899f3d1b2b49990baaa4cc
Cefi Halegua <cefi93@hotmail.com>
2019-06-03 16:40:49 +0300

    random distribution completed

--- js/memory-game.js
+++ js/memory-game.js
@@ -1,8 +1,8 @@
-//randomly distributing the cards
+//randomly choosing pictures and distributing the cards
 var numberOfCards = 12; //total number of cards in the board
 var numberOfPictures = 10; //number of different pictures to choose cards
 var pictures = [];
-function choosePictures() {
+function choosingPictures() {
     var picturesArray = [];
     for(let i=0; i<numberOfPictures; i++) {
         picturesArray[i] = i;
@@ -14,5 +14,18 @@ function choosePictures() {
         chosenPictures[j+(numberOfCards/2)] = picturesArray[chosenIndex];
         picturesArray.splice(chosenIndex, 1);
     }
-    console.log(chosenPictures);
+    distributeCards(chosenPictures);
 }
+function distributeCards(chosenPictures) {
+    var pictureDistributionOrder = [];
+    for(let k=0; k<numberOfCards; k++) {
+        let chosenIndex = Math.floor(Math.random()*chosenPictures.length);
+        pictureDistributionOrder[k] = chosenPictures[chosenIndex];
+        chosenPictures.splice(chosenIndex, 1);
+    }
+    for (let n=0; n<numberOfCards; n++) {
+        $("#card" + (n+1)).prepend("<img class='card-front' id='image" + (n+1) + "' src='./images/ca" + (pictureDistributionOrder[n] + 1) + ".jpg' />");
+        $("#card" + (n+1)).prepend("<img class='card-back' id='back" + (n+1) + "' src='./images/backcard.jpg' />");
+    }
+}
+choosingPictures();
